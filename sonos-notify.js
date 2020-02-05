80b0070440ed18af1e7c4d7d2885ac0f2e8bbb31
pokiiio <32353873+pokiiio@users.noreply.github.com>
2020-02-05 12:06:30 +0900

    Fix a bug.

--- sonos-notify.js
+++ sonos-notify.js
@@ -4,30 +4,36 @@ module.exports = function (RED) {
         var node = this;
         var address = config.address;
         var port = config.port;
-        var lastUri = '';
         var Sonos = require('sonos');
 
         if (!address || !port) {
             Sonos.DeviceDiscovery((device) => {
                 address = device.host;
                 port = device.port;
-
-                var device = new Sonos.Sonos(address, port);
-
-                device.on('CurrentTrack', (track) => {
-                    if (!track || track.uri === lastUri) {
-                        return;
-                    }
-
-                    lastUri = track.uri;
-
-                    var msg = {};
-                    msg.event = 'CurrentTrack';
-                    msg.payload = track;
-                    node.send(msg);
-                })
+                registerListener(node, address, port);
             })
         }
+
+        registerListener(node, address, port);
     }
+
+    function registerListener(node, address, port) {
+        var device = new Sonos.Sonos(address, port);
+        var lastUri = '';
+
+        device.on('CurrentTrack', (track) => {
+            if (!track || track.uri === lastUri) {
+                return;
+            }
+
+            lastUri = track.uri;
+
+            var msg = {};
+            msg.event = 'CurrentTrack';
+            msg.payload = track;
+            node.send(msg);
+        })
+    }
+
     RED.nodes.registerType("sonos-notify", SonosNotifyNode);
 }
